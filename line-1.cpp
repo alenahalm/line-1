8480aa25813dbc2ad0cc4cdc2e81ee3ca42621b8
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:15:17 +0800

    Initial commit

---
+++ line-1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int deg, min, sec;
+	long double rad;
+	cout << "Enter degrees, minutes and seconds" << endl;
+	cin >> deg >> min >> sec;
+	sec += min * 60 + deg * 3600;
+	rad = (sec * M_PI) / (180.0 * 3600.0);
+	cout << setprecision(15) << rad << endl;
+	system("pause");
+}
